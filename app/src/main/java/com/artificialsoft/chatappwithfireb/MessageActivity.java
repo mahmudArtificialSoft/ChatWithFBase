34acfcf31cfa4aa043fe3c69f2b98e236b91608f
unknown <mahmud@artificial-soft.com>
2019-06-15 18:25:18 +0600

    tutorial 8 complete

--- app/src/main/java/com/artificialsoft/chatappwithfireb/MessageActivity.java
+++ app/src/main/java/com/artificialsoft/chatappwithfireb/MessageActivity.java
@@ -6,7 +6,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.artificialsoft.chatappwithfireb.models.User;
 import com.bumptech.glide.Glide;
@@ -17,6 +20,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -29,6 +34,9 @@ public class MessageActivity extends AppCompatActivity {
     DatabaseReference reference;
 
     Intent intent;
+
+    ImageButton btn_send;
+    EditText text_send;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,17 +57,33 @@ public class MessageActivity extends AppCompatActivity {
         profile_Image = findViewById(R.id.profile_image);
         username = findViewById(R.id.tv_userName);
 
-        intent = getIntent();
-        String userID = intent.getStringExtra("userid");
+        btn_send = findViewById(R.id.btn_send);
+        text_send = findViewById(R.id.text_send);
 
+        intent = getIntent();
+        final String userID = intent.getStringExtra("userid");
         fuser = FirebaseAuth.getInstance().getCurrentUser();
+
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg = text_send.getText().toString().trim();
+                if (!msg.equals("")){
+                    sendMessage(fuser.getUid(), userID, msg);
+                } else {
+                    Toast.makeText(MessageActivity.this, "Your can't send empty message!", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+
         reference = FirebaseDatabase.getInstance().getReference("Users").child(userID);
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 User user = dataSnapshot.getValue(User.class);
                 username.setText(user.getUsername());
-                if (user.getImageURL().equals("default")){
+                if (user.getImageURL().equals("default")) {
                     profile_Image.setImageResource(R.mipmap.ic_launcher);
                 } else {
                     Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_Image);
@@ -71,5 +95,16 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void sendMessage(String sender, String receiver, String message) {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> hashMap = new HashMap<>();
+
+        hashMap.put("sender", sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference.child("chats").push().setValue(hashMap);
     }
 }
